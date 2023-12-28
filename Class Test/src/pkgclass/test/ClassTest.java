fad2a19dcc8079976a61f69b32ce219c40022100
Parves Mosharuf <97355947+parves01@users.noreply.github.com>
2023-12-28 22:39:46 +0600

    Show all shortcut.(+-*/)
    step by step

---
+++ Class Test/src/pkgclass/test/ClassTest.java
@@ -0,0 +1,44 @@
+package pkgclass.test;
+/*
+PARVES MOSHARUF
+*/
+public class ClassTest
+        
+{
+    public static void main(String[] args)
+    
+    
+    {
+        
+        int P;
+        P=10;
+        P=P+1;
+        P+=1;
+        P++;
+        
+        System.out.println(P);
+        
+        int p;
+        p=10;
+        p=p-1;
+        p-=1;
+        p--;
+        
+        System.out.println(p);
+        
+        int M;
+        M=10;
+        M=M*1;
+        M*=1;
+        
+        System.out.println(M);
+        
+        int m;
+        m=10;
+        m=m/1;
+        m/=1;
+        
+        System.out.println(m);
+        
+    }
+}
