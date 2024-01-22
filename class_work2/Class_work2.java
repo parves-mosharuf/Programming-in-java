90254c92fe625e536bab0568796a7fae35a5ef14
Parves Mosharuf <97355947+parves01@users.noreply.github.com>
2024-01-22 11:26:53 +0600

    Add files via upload
    LeapYear

---
+++ class_work2/Class_work2.java
@@ -0,0 +1,21 @@
+package class_work2;
+import java.util.Scanner;
+
+public class Class_work2 
+{
+
+    public static void main(String[] args)
+    {
+        Scanner leapyear = new Scanner(System.in);
+        System.out.print("Enter year : ");
+        int year = leapyear.nextInt();
+        if (year%4==0)
+        {
+            System.out.println("leap year");
+        }
+        else 
+        {
+            System.out.println("is not a leap year");
+        }
+    }
+}
