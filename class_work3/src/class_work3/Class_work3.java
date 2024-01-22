8747a5c02b9e28cbd995a379d9f5fbead7cf958c
Parves Mosharuf <97355947+parves01@users.noreply.github.com>
2024-01-22 19:57:54 +0600

    Add files via upload

---
+++ class_work3/src/class_work3/Class_work3.java
@@ -0,0 +1,14 @@
+package class_work3;
+public class Class_work3 {
+    //                                                  parves                  mosharof                    
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 0;
+
+        for (int i = 1; i <=a; i++) {
+            b += i;
+        }
+
+        System.out.println(+ b);
+    }
+}
